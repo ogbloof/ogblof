b5dab644697eceeeb1c1c3deaf0a82225b92ce47
ogbloof <117357217+ogbloof@users.noreply.github.com>
2024-04-03 14:20:38 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Удаление анимации загрузки после полной загрузки страницы
+    document.querySelector(".loader-wrapper").style.display = "none";
+});
