657837af4561450b903a02a45af40e989be7f1ec
ogbloof <117357217+ogbloof@users.noreply.github.com>
2024-04-16 17:57:58 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,40 +1,15 @@
-function redirectToCoinGame() {
-    window.location.href = "coin_game.html";
-}
+document.addEventListener("DOMContentLoaded", function() {
+    // Генерация случайного количества монет
+    var numberOfCoins = Math.floor(Math.random() * 20) + 10; // От 10 до 30 монет
 
-function flipCoin() {
-    var coin = document.getElementById('coin');
-    coin.classList.add('flip-animation');
+    // Создание и добавление монет на страницу
+    var coinsContainer = document.getElementById('coins');
+    for (var i = 0; i < numberOfCoins; i++) {
+        var coin = document.createElement('div');
+        coin.classList.add('coin');
+        coin.style.left = Math.random() * 100 + 'vw'; // Случайное горизонтальное положение монеты
+        coin.style.animation = 'fall ' + (Math.random() * 2 + 1) + 's linear ' + (Math.random() * 2) + 's infinite'; // Случайная длительность анимации и задержка
+        coinsContainer.appendChild(coin);
+    }
+});
 
-    setTimeout(function() {
-        coin.classList.remove('flip-animation');
-        var result = Math.random() < 0.5 ? 'Heads' : 'Tails';
-        alert('You got ' + result);
-        showButtons(); // Показываем кнопки после завершения броска монеты
-    }, 500); // Adjust the timeout to match the duration of the flip animation
-}
-
-var chosenSide = '';
-
-function chooseSide(side) {
-    chosenSide = side;
-    var buttons = document.querySelectorAll('.button');
-    buttons.forEach(function(button) {
-        button.style.display = 'none';
-    });
-    var coin = document.getElementById('coin');
-    coin.style.backgroundImage = "url('coin.png')";
-    coin.classList.add('flip-animation');
-    setTimeout(flipCoin, 2000); // Имитация подбрасывания монетки после 2 секунд
-}
-
-function showButtons() {
-    var playAgainButton = document.getElementById('playAgainButton');
-    var returnToMenuButton = document.getElementById('returnToMenuButton');
-    playAgainButton.style.display = 'block';
-    returnToMenuButton.style.display = 'block';
-}
-
-function returnToMenu() {
-    window.location.href = "index.html";
-}
